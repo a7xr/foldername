61a9060148ad573a0eeda6331a16f0adc9d8677c
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 18:38:30 +0300

    UseState Hook Simple print

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, { useState } from "react";
 
-// https://www.geeksforgeeks.org/what-is-usestate-in-react/?ref=gcse
-function App(props) {
-  const [count, setRandomCount] = useState(function generateRandomInteger() {
-    return Math.floor(Math.random() * 100);
-  });
-  function clickHandler(e) {
-    setRandomCount(Math.floor(Math.random() * 100));
-  }
-  return (
-    <div style={{ margin: "auto", width: 100, display: "block" }}>
-      <h1> {count} </h1>
-
-      <p>
-        <button onClick={clickHandler}> Click </button>
-      </p>
-    </div>
-  );
+// https://www.geeksforgeeks.org/reactjs-usestate-hook/?ref=gcse
+function App() {
+  const click = useState("GeeksForGeeks");
+  return <h1>Welcome to {click}</h1>;
 }
 
 export default App;
