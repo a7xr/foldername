158573350468f8821121f34c91c6c7051619281f
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 17:13:20 +0300

    SetState 001

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return <h1> Hello World! </h1>;
+class App extends Component {
+  // Previous Hello World: https://www.geeksforgeeks.org/build-a-basic-react-app-that-display-hello-world/
+  // This SetState: https://www.geeksforgeeks.org/reactjs-setstate/?ref=leftbar-rightbar
+  constructor(props) {
+    super(props);
+
+    // Set initial state
+    this.state = { greeting: "Click the button to receive greetings" };
+
+    // Binding this keyword
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    // Changing state
+    this.setState({ greeting: "GeeksForGeeks welcomes you !!" });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Greetings Portal</h2>
+        <p>{this.state.greeting}</p>
+
+        {/* Set click handler */}
+        <button onClick={this.updateState}>Click me!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
