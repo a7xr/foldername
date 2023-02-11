66b6b198741abcfbf9e09c43e76fcf8e0b29d72e
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 18:39:03 +0300

    UseState Hook Increment on click

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { useState } from "react";
 
 // https://www.geeksforgeeks.org/reactjs-usestate-hook/?ref=gcse
 function App() {
-  const click = useState("GeeksForGeeks");
-  return <h1>Welcome to {click}</h1>;
+  const [click, setClick] = useState(0);
+  // using array destructuring here
+  // to assign initial value 0
+  // to click and a reference to the function
+  // that updates click to setClick
+  return (
+    <div>
+      <p>You clicked {click} times</p>
+
+      <button onClick={() => setClick(click + 1)}>Click me</button>
+    </div>
+  );
 }
 
 export default App;
