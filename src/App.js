c593c952784ebbff86cc633e444b368f0efa5753
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 19:02:55 +0300

    Form in React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,130 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-// https://www.geeksforgeeks.org/what-is-usestate-in-react/?ref=gcse
-function App(props) {
-  const [count, setRandomCount] = useState(function generateRandomInteger() {
-    return Math.floor(Math.random() * 100);
-  });
-  function clickHandler(e) {
-    setRandomCount(Math.floor(Math.random() * 100));
-  }
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+
+  // function to update state of name with
+  // value enter by user in form
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  // function to update state of age with value
+  // enter by user in form
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+  // function to update state of email with value
+  // enter by user in form
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  // function to update state of password with
+  // value enter by user in form
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  // function to update state of confirm password
+  // with value enter by user in form
+  const handleConfPasswordChange = (e) => {
+    setConfPassword(e.target.value);
+  };
+  // below function will be called when user
+  // click on submit button .
+  const handleSubmit = (e) => {
+    if (password != confPassword) {
+      // if 'password' and 'confirm password'
+      // does not match.
+      alert("password Not Match");
+    } else {
+      // display alert box with user
+      // 'name' and 'email' details .
+      alert(
+        'A form was submitted with Name :"' +
+          name +
+          '" ,Age :"' +
+          age +
+          '" and Email :"' +
+          email +
+          '"'
+      );
+    }
+    e.preventDefault();
+  };
   return (
-    <div style={{ margin: "auto", width: 100, display: "block" }}>
-      <h1> {count} </h1>
-
-      <p>
-        <button onClick={clickHandler}> Click </button>
-      </p>
+    <div className="App">
+      <header className="App-header">
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <h2> Geeks For Geeks </h2>
+          <h3> Sign-up Form </h3>
+          <img src="/image001.png" />
+          <label>Name:</label>
+          <br />
+          <input
+            type="text"
+            value={name}
+            required
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+          <br />
+          <label>Age:</label>
+          <br />
+          <input
+            type="text"
+            value={age}
+            required
+            onChange={(e) => {
+              handleAgeChange(e);
+            }}
+          />
+          <br />
+          <label>Email:</label>
+          <br />
+          <input
+            type="email"
+            value={email}
+            required
+            onChange={(e) => {
+              handleEmailChange(e);
+            }}
+          />
+          <br />
+          <label>Password:</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            required
+            onChange={(e) => {
+              handlePasswordChange(e);
+            }}
+          />
+          <br />
+          <label>Confirm Password:</label>
+          <br />
+          <input
+            type="password"
+            value={confPassword}
+            required
+            onChange={(e) => {
+              handleConfPasswordChange(e);
+            }}
+          />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </header>
     </div>
   );
 }
