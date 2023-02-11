2ab95ddbda038db19373ea2148479044a7b0c05d
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 17:19:14 +0300

    SetState update state values using pros

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  static defaultProps = {
+    testTopics: [
+      "React JS",
+      "Node JS",
+      "Compound components",
+      "Lifecycle Methods",
+      "Event Handlers",
+      "Router",
+      "React Hooks",
+      "Redux",
+      "Context",
+    ],
+  };
+
   constructor(props) {
     super(props);
 
     // Set initial state
     this.state = {
-      test: "Nil",
-      questions: "0",
-      students: "0",
+      testName: "React js Test",
+      topics: "",
     };
 
     // Binding this keyword
     this.updateState = this.updateState.bind(this);
   }
 
+  listOfTopics() {
+    return (
+      <ul>
+        {this.props.testTopics.map((topic) => (
+          <li>{topic}</li>
+        ))}
+      </ul>
+    );
+  }
+
   updateState() {
     // Changing state
     this.setState({
-      test: "Programming Quiz",
-      questions: "10",
-      students: "30",
+      testName: "Test topics are:",
+      topics: this.listOfTopics(),
     });
   }
 
   render() {
     return (
       <div>
-        <h2>Test Portal</h2>
-        <p>{this.state.test}</p>
-
-        <p>{this.state.questions}</p>
-
-        <p>{this.state.students}</p>
+        <h2>Test Information</h2>
+        <p>{this.state.testName}</p>
+        <p>{this.state.topics}</p>
         {/* Set click handler */}
         <button onClick={this.updateState}>Click me!</button>
       </div>
