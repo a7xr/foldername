a25bf895a00d7ed321db52c122c9c0024bf4d6d5
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 18:39:51 +0300

    UseState Hook Increment on click + test even|odd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
-// https://www.geeksforgeeks.org/reactjs-usestate-hook/?ref=gcse
 function App() {
   const [click, setClick] = useState(0);
-  // using array destructuring here
-  // to assign initial value 0
-  // to click and a reference to the function
-  // that updates click to setClick
+
   return (
     <div>
-      <p>You clicked {click} times</p>
+      <p>You've clicked {click} times!</p>
 
-      <button onClick={() => setClick(click + 1)}>Click me</button>
+      <p>
+        The number of times you have clicked is{" "}
+        {click % 2 == 0 ? "even!" : "odd!"}
+      </p>
+
+      <button onClick={() => setClick((click) => click + 1)}>Click me</button>
     </div>
   );
 }
