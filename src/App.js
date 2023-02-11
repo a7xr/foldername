6a5ae14cde6018b65e371fa28a668c3d6b5b05a4
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 18:34:52 +0300

    UseState 001

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-// https://www.geeksforgeeks.org/when-to-use-usecallback-usememo-and-useeffect/?ref=leftbar-rightbar
-
-function App() {
-  /* Some data */
-  const data = {
-    Colors: ["red", "green", "yellow"],
-    Fruits: ["Apple", "mango", "Banana"],
-  };
-
-  /* Initial states */
-  const [currentChoice, setCurrentChoice] = useState("Colors");
-  const [items, setItems] = useState([]);
-
-  /* Using useEffect to set the data of currentchoice
-       to items and console log the fetching... */
-  useEffect(() => {
-    setItems(data[currentChoice]);
-    console.log("Data is fetched!");
-  }, [currentChoice]);
-
+// https://www.geeksforgeeks.org/what-is-usestate-in-react/?ref=gcse
+function App(props) {
+  const [count, setRandomCount] = useState(function generateRandomInteger() {
+    return Math.floor(Math.random() * 100);
+  });
+  function clickHandler(e) {
+    setRandomCount(Math.floor(Math.random() * 100));
+  }
   return (
-    <>
-      <button onClick={() => setCurrentChoice("Colors")}>Colors</button>
-      <button onClick={() => setCurrentChoice("Fruits")}>Fruits</button>
-      {items.map((item) => {
-        return <div key={item}>{item}</div>;
-      })}
-    </>
+    <div style={{ margin: "auto", width: 100, display: "block" }}>
+      <h1> {count} </h1>
+
+      <p>
+        <button onClick={clickHandler}> Click </button>
+      </p>
+    </div>
   );
 }
 
