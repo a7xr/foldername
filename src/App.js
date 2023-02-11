8b5cada44a2038a45a931b4f11f6acd2aebdfab4
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 17:20:39 +0300

    SetState update state using its prev value

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  static defaultProps = {
-    testTopics: [
-      "React JS",
-      "Node JS",
-      "Compound components",
-      "Lifecycle Methods",
-      "Event Handlers",
-      "Router",
-      "React Hooks",
-      "Redux",
-      "Context",
-    ],
-  };
-
   constructor(props) {
     super(props);
 
     // Set initial state
     this.state = {
-      testName: "React js Test",
-      topics: "",
+      count: 0,
     };
 
     // Binding this keyword
     this.updateState = this.updateState.bind(this);
   }
 
-  listOfTopics() {
-    return (
-      <ul>
-        {this.props.testTopics.map((topic) => (
-          <li>{topic}</li>
-        ))}
-      </ul>
-    );
-  }
-
   updateState() {
     // Changing state
-    this.setState({
-      testName: "Test topics are:",
-      topics: this.listOfTopics(),
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
     });
   }
 
   render() {
     return (
       <div>
-        <h2>Test Information</h2>
-        <p>{this.state.testName}</p>
-        <p>{this.state.topics}</p>
+        <h2>Click Counter</h2>
+        <p>You have clicked me {this.state.count} times.</p>
         {/* Set click handler */}
         <button onClick={this.updateState}>Click me!</button>
       </div>
