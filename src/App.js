7413a37445539c84a495cee0134ae2ab5cbb3ac4
Mamitiana <mamitiana@fulldigits.com>
2023-02-11 17:14:46 +0300

    SetState Plusieurs Attributs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  // Previous Hello World: https://www.geeksforgeeks.org/build-a-basic-react-app-that-display-hello-world/
-  // This SetState: https://www.geeksforgeeks.org/reactjs-setstate/?ref=leftbar-rightbar
   constructor(props) {
     super(props);
 
     // Set initial state
-    this.state = { greeting: "Click the button to receive greetings" };
+    this.state = {
+      test: "Nil",
+      questions: "0",
+      students: "0",
+    };
 
     // Binding this keyword
     this.updateState = this.updateState.bind(this);
@@ -15,15 +17,22 @@ class App extends Component {
 
   updateState() {
     // Changing state
-    this.setState({ greeting: "GeeksForGeeks welcomes you !!" });
+    this.setState({
+      test: "Programming Quiz",
+      questions: "10",
+      students: "30",
+    });
   }
 
   render() {
     return (
       <div>
-        <h2>Greetings Portal</h2>
-        <p>{this.state.greeting}</p>
+        <h2>Test Portal</h2>
+        <p>{this.state.test}</p>
 
+        <p>{this.state.questions}</p>
+
+        <p>{this.state.students}</p>
         {/* Set click handler */}
         <button onClick={this.updateState}>Click me!</button>
       </div>
