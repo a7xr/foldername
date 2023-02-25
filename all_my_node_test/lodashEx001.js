7d6d3de4e925dfe37843b4e9e01c3e65d7b39d6c
Mamitiana <mamitiana@fulldigits.com>
2023-02-25 18:45:13 +0300

    lodash usage of _.times

--- all_my_node_test/lodashEx001.js
+++ all_my_node_test/lodashEx001.js
@@ -1,7 +1,16 @@
-const _ = require('lodash');
+// https://medium.com/voobans-tech-stories/10-lodash-functions-everyone-should-know-334b372aec5d
+
+const _ = require("lodash");
 
 var foo = { a: "a property" };
-var bar = { b: 4, c: "an other property" }
+var bar = { b: 4, c: "an other property" };
 
 var result = _.assign({ a: "an old property" }, foo, bar);
+// console.log(result);
+
+function getRandomInteger() {
+  return Math.round(Math.random() * 100);
+}
+
+var result = _.times(5, getRandomInteger);
 console.log(result);
